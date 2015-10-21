ad93b51dc3efc5617b5d5500c2d2e9efefec5729
vladmeh <vladmeh@gmail.com>
2015-10-21 17:55:40 +0300

    home page

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,54 +1,56 @@
 <?php
 
 /* @var $this yii\web\View */
+use yii\helpers\Html;
 
 $this->title = 'My Yii Application';
 ?>
 
-<div class="site-index">
+<div class="intro column">
+    <h1 class="hide">Торговый дом "Альфа"</h1>
+    <div class="row center"><?=Html::img('/images/logo.png',['class'=>'logo'])?></div>
+</div>
 
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
-        </div>
-
+<div class="card-panel p3">
+    <div class="card-content">
+        <h4 class="card-title grey-text text-darken-3 center-align mt0 mb2">Добро пожаловать в Торговый дом "Альфа"</h4>
+        <p class="mb1">Уважаемые посетители интернет сайта Торгового дома "Альфа"! Мы рады приветствовать Вас на своем интернет ресурсе. Наш интернет сайт создан для удобства поиска товаров и услуг, получения информации о мероприятиях, проводимых в ТД "Альфа", а так же информации о наличии свободных площадей.</p>
+        <p class="mb1">Торговый дом "Альфа" это 1500 кв.м., на которой расположены разнообразные магазины и центры услуг, где можно приобрести все необходимое для жизни. Множество больших и маленьких магазинов с товарами на любой вкус от продуктового магазина до парикмахерской, от аптеки до магазина игрушек.</p>
+        <p class="mb1">Мы всегда рады видеть Вас в ТД "Альфа" в качестве покупателей и арендаторов.</p>
+        <p class="mb1 right-align">С уважением, коллектив ТД "Альфа".</p>
     </div>
 </div>
+
+<!--<div class="site-index">
+
+    <div class="row">
+        <div class="col s4">
+            <h2>Heading</h2>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+
+            <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+        </div>
+        <div class="col s4">
+            <h2>Heading</h2>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat nulla pariatur.</p>
+
+            <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+        </div>
+        <div class="col s4">
+            <h2>Heading</h2>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat nulla pariatur.</p>
+
+            <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+        </div>
+    </div>
+
+</div>-->
